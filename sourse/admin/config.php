68a0924d0a492339b9b5c440c95af3293eb4f7fa
AlexYuriy <vlolipops@gmail.com>
2014-07-01 21:34:20 +0400

    В папке sourse исходники сайта, так же прикрепил экспорт из бд.

---
+++ sourse/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.istochnik-spb.com/admin/');
+define('HTTP_CATALOG', 'http://test.istochnik-spb.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.istochnik-spb.com/admin/');
+define('HTTPS_CATALOG', 'http://test.istochnik-spb.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1245282/istochnik-spb.com/htdocs/test/admin/');
+define('DIR_SYSTEM', '/home/host1245282/istochnik-spb.com/htdocs/test/system/');
+define('DIR_DATABASE', '/home/host1245282/istochnik-spb.com/htdocs/test/system/database/');
+define('DIR_LANGUAGE', '/home/host1245282/istochnik-spb.com/htdocs/test/admin/language/');
+define('DIR_TEMPLATE', '/home/host1245282/istochnik-spb.com/htdocs/test/admin/view/template/');
+define('DIR_CONFIG', '/home/host1245282/istochnik-spb.com/htdocs/test/system/config/');
+define('DIR_IMAGE', '/home/host1245282/istochnik-spb.com/htdocs/test/image/');
+define('DIR_CACHE', '/home/host1245282/istochnik-spb.com/htdocs/test/system/cache/');
+define('DIR_DOWNLOAD', '/home/host1245282/istochnik-spb.com/htdocs/test/download/');
+define('DIR_LOGS', '/home/host1245282/istochnik-spb.com/htdocs/test/system/logs/');
+define('DIR_CATALOG', '/home/host1245282/istochnik-spb.com/htdocs/test/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1245282_test');
+define('DB_PASSWORD', 'test');
+define('DB_DATABASE', 'host1245282_test');
+define('DB_PREFIX', '');
+?>
