71c6afb000da56ddd1d078d03be78a7e84302c3d
yuriiq <yuriiq>
2014-08-23 14:57:49 +0400

    Фикс ошибки language_id
    Signed-off-by: yuriiq <yuriiq>

--- test_yuriiq/catalog/controller/module/blog_reviews.php
+++ test_yuriiq/catalog/controller/module/blog_reviews.php
@@ -2,54 +2,44 @@
 class ControllerModuleBlogReviews extends Controller {
 	protected function index($setting) {
 		$this->language->load('module/blog_reviews');
-
 		$this->load->model('blog/article');
-		
 		$this->load->model('tool/image');
-		
 		$this->load->model('module/blog_reviews');
-		
 		$this->data['text_views'] = $this->language->get('text_views');
-		if (strlen($setting['header'][$this->config->get('config_language_id')]) > 0){
+		$language_id = $this->config->get('config_language_id');
+		if (isset ($setting['header'][$language_id]) && strlen($setting['header'][$language_id]) > 0) {
       		$this->data['header'] = $setting['header'][$this->config->get('config_language_id')];
 		} else {
       		$this->data['header'] = false;
 		}
-		
 		$this->data['reviews'] = array();
-
 		if ($setting['limit'] > 0) {
 			$limit = $setting['limit'];
 		} else {
 			$limit = 4;
 		}
-		
 		if ($setting['text_limit'] > 0) {
 			$text_limit = $setting['text_limit'];
 		} else {
 			$text_limit = 50;
 		}
-		
 		if ($setting['type'] == 'latest') {
 			$results = $this->model_module_blog_reviews->getLatestReviews($limit);
 		} else {
 			$results = $this->model_module_blog_reviews->getRandomReviews($limit);
 		}
-
 		foreach ($results as $result) {
 			if ($this->config->get('config_review_article_status')) {
 				$rating = $result['rating'];
 			} else {
 				$rating = false;
 			}
-
    			$article_id = false;
    			$article = false;
    			$articl_thumb = false;
    			$articl_name = false;
 			$articl_viewed = false;
    			$articl_href = false;
-			
 			if ($result['article_id']) {
 				$article = $this->model_blog_article->getArticle($result['article_id']);
 				if ($article['image']) {
@@ -60,7 +50,6 @@ class ControllerModuleBlogReviews extends Controller {
 				$articl_viewed = $article['viewed'];
     			$articl_href = $this->url->link('blog/article', 'article_id=' . $article['article_id']);
 			}
-
 			$this->data['reviews'][] = array(
 				'review_id'   => $result['review_article_id'],
 				'rating'      => $result['rating'],
@@ -75,17 +64,13 @@ class ControllerModuleBlogReviews extends Controller {
   				'articl_href'   => $articl_href
 			);
 		}
-
 		if ((file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/blog_reviews.tpl'))and (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/blog_reviews_middle.tpl')))     {
 			$this->template = $this->config->get('config_template') . '/template/module/blog_reviews.tpl';
 			
 			if (($setting['position']=='content_top') or ($setting['position']=='content_bottom'))  {$this->template = $this->config->get('config_template') . '/template/module/blog_reviews_middle.tpl';};
-			
-		
 		} else {
 			$this->template = 'default/template/module/blog_reviews.tpl';
 		}
-
 		$this->render();
 	}
 }
