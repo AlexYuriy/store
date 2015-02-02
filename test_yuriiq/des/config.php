dc332b15ca5ea8573186757b1adabd19c94c42db
AlexYuriy <vlolipops@gmail.com>
2015-02-02 12:44:04 +0300

    fix fix fix this
    Signed-off-by: AlexYuriy <vlolipops@gmail.com>

---
+++ test_yuriiq/des/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://vteple.net/des/');
+define('HTTP_CATALOG', 'http://vteple.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://vteple.net/des/');
+define('HTTPS_CATALOG', 'http://vteple.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home/k18379/public_html/vteple.net/des/');
+define('DIR_SYSTEM', '/home/k18379/public_html/vteple.net/system/');
+define('DIR_DATABASE', '/home/k18379/public_html/vteple.net/system/database/');
+define('DIR_LANGUAGE', '/home/k18379/public_html/vteple.net/des/language/');
+define('DIR_TEMPLATE', '/home/k18379/public_html/vteple.net/des/view/template/');
+define('DIR_CONFIG', '/home/k18379/public_html/vteple.net/system/config/');
+define('DIR_IMAGE', '/home/k18379/public_html/vteple.net/image/');
+define('DIR_CACHE', '/home/k18379/public_html/vteple.net/system/cache/');
+define('DIR_DOWNLOAD', '/home/k18379/public_html/vteple.net/download/');
+define('DIR_LOGS', '/home/k18379/public_html/vteple.net/system/logs/');
+define('DIR_CATALOG', '/home/k18379/public_html/vteple.net/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'k18379_dbuser');
+define('DB_PASSWORD', '-zYFyQcR');
+define('DB_DATABASE', 'k18379_db');
+define('DB_PREFIX', 'oc_prefix_');
+?>
--- test_yuriiq/manager/config.php
+++ test_yuriiq/manager/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://test_yuriiq.istochnik-spb.com/manager/');
-define('HTTP_CATALOG', 'http://test_yuriiq.istochnik-spb.com/');
+define('HTTP_SERVER', 'http://vteple.net/manager/');
+define('HTTP_CATALOG', 'http://vteple.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://test_yuriiq.istochnik-spb.com/manager/');
-define('HTTPS_CATALOG', 'http://test_yuriiq.istochnik-spb.com/');
+define('HTTPS_SERVER', 'http://vteple.net/manager/');
+define('HTTPS_CATALOG', 'http://vteple.net/');
 
 // DIR
-define('DIR_APPLICATION', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/manager/');
-define('DIR_SYSTEM', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/system/');
-define('DIR_DATABASE', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/system/database/');
-define('DIR_LANGUAGE', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/manager/language/');
-define('DIR_TEMPLATE', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/manager/view/template/');
-define('DIR_CONFIG', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/system/config/');
-define('DIR_IMAGE', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/image/');
-define('DIR_CACHE', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/system/cache/');
-define('DIR_DOWNLOAD', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/download/');
-define('DIR_LOGS', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/system/logs/');
-define('DIR_CATALOG', '/home/host1245282/istochnik-spb.com/htdocs/test_yuriiq/catalog/');
+define('DIR_APPLICATION', '/home/k18379/public_html/vteple.net/manager/');
+define('DIR_SYSTEM', '/home/k18379/public_html/vteple.net/system/');
+define('DIR_DATABASE', '/home/k18379/public_html/vteple.net/system/database/');
+define('DIR_LANGUAGE', '/home/k18379/public_html/vteple.net/manager/language/');
+define('DIR_TEMPLATE', '/home/k18379/public_html/vteple.net/manager/view/template/');
+define('DIR_CONFIG', '/home/k18379/public_html/vteple.net/system/config/');
+define('DIR_IMAGE', '/home/k18379/public_html/vteple.net/image/');
+define('DIR_CACHE', '/home/k18379/public_html/vteple.net/system/cache/');
+define('DIR_DOWNLOAD', '/home/k18379/public_html/vteple.net/download/');
+define('DIR_LOGS', '/home/k18379/public_html/vteple.net/system/logs/');
+define('DIR_CATALOG', '/home/k18379/public_html/vteple.net/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'host1245282_test_yuriiq');
-define('DB_PASSWORD', 'test_yuriiq');
-define('DB_DATABASE', 'host1245282_testYuriiq');
+define('DB_USERNAME', 'k18379_dbuser');
+define('DB_PASSWORD', '-zYFyQcR');
+define('DB_DATABASE', 'k18379_db');
 define('DB_PREFIX', 'oc_prefix_');
 ?>
