a51a24c4d0cf48b51183d2434ff51a32c719716a
Alex <vlolipops@gmail.com>
2015-06-07 12:39:26 +0300

    Обновление нового сайта

--- test_yuriiq/admin/model/module/downloads.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ModelModuleDownloads extends Model {
-	public function initDownload() {
-		$query = $this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX . "category_to_download (category_id INT(11), download_id INT(11), PRIMARY KEY (category_id, download_id))");
-	}	
-}
-?>
